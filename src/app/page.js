aeddf70bee3e45f5030b5ae8ed3db1b86035cf78
fahrifzgg <fahrifz.gg@gmail.com>
2023-09-13 15:59:18 +0700

    first commit

---
+++ src/app/page.js
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1>Home Page</h1>
+      <Link href="/downloads/Zadna.xlsx" download>
+        download
+      </Link>
+    </main>
+  );
+}
