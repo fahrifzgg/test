42d17393efd0bc0012ae7cd4c63dad755e50dec1
fahrifzgg <fahrifz.gg@gmail.com>
2023-09-13 16:11:43 +0700

    change download button

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,11 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>Home Page</h1>
-      <Link href="/downloads/Zadna.xlsx" download>
+      <Link
+        className="px-4 py-2 bg-white text-black rounded-md"
+        href="/downloads/Zadna.xlsx"
+        download
+      >
         download
       </Link>
     </main>
