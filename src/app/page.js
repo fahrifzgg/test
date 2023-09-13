02f412d620fe6e772bcf85052e0f2be4e66e8929
fahrifz <fahrifauzi15@gmail.com>
2023-09-13 17:58:07 +0700

    Add new feature

--- src/app/page.js
+++ src/app/page.js
@@ -2,14 +2,14 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center p-24">
       <h1>Home Page</h1>
       <Link
         className="px-4 py-2 bg-white text-black rounded-md"
         href="/downloads/Zadna.xlsx"
         download
       >
-        download
+        Download
       </Link>
     </main>
   );
