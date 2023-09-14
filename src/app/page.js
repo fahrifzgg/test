8343b5cbe9824a501abf7418b9dcd3b66515d7e8
fahrifzgg <fahrifz.gg@gmail.com>
2023-09-14 09:46:37 +0700

    change home screen

--- src/app/page.js
+++ src/app/page.js
@@ -2,15 +2,26 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center p-24">
+    <main className="min-h-screen">
+      <nav className="bg-white text-black flex justify-between px-6 py-3">
+        <div>
+          <h1>LOGO</h1>
+        </div>
+        <div className="flex">
+          <ul className="flex space-x-3">
+            <li>
+              <Link href="/">Home</Link>
+            </li>
+            <li>
+              <Link href="/">Product</Link>
+            </li>
+            <li>
+              <Link href="/">About</Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
       <h1>Home Page</h1>
-      <Link
-        className="px-4 py-2 bg-white text-black rounded-md"
-        href="/downloads/Zadna.xlsx"
-        download
-      >
-        Download
-      </Link>
     </main>
   );
 }
